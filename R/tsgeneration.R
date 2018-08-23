85988c093152a052fb8d8de1efcf6eae708eca4e
Rob J Hyndman <robjhyndman@gmail.com>
2018-08-23 10:31:00 +1000

    Fixed imports

--- R/tsgeneration.R
+++ R/tsgeneration.R
@@ -6,7 +6,7 @@
 #' @name tsgeneration
 #' @importFrom stats as.ts coef dlnorm plnorm qlnorm rlnorm rmultinom rnorm runif stl ts
 #' @importFrom stats diffinv fivenum frequency na.exclude na.omit sd window
-#' @importFrom utils head write.csv str flush.console
+#' @importFrom utils head write.csv str flush.console globalVariables
 #' @importFrom graphics points polygon title
 #' @importFrom grDevices adjustcolor
 #' @importFrom methods new slotNames
@@ -17,9 +17,14 @@
 #' @importFrom forecast trendcycle msts autoplot
 #' @importFrom dplyr mutate select bind_cols
 #' @importFrom magrittr "%>%"
-#' @importFrom foreach foreach "%dopar%"
+#' @importFrom foreach foreach "%dopar%" "%do%"
 #' @importFrom GA ga gaControl gaMonitor
 #' @importFrom GA startParallel garun
+#' @importFrom tsfeatures tsfeatures
+#' @importFrom doRNG "%dorng%"
 #' @import shiny
 NULL
-#> NULL
+# > NULL
+
+
+utils::globalVariables(c(".","x","Season2"))
