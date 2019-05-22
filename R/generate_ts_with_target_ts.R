f4c0950d6627b45d45b41c91d657bacd53ffb626
Yanfei Kang <yanfei.kang@outlook.com>
2019-05-22 20:40:12 +0800

    change GA init

--- R/generate_ts_with_target_ts.R
+++ R/generate_ts_with_target_ts.R
@@ -3,7 +3,9 @@ generate_ts_with_target_ts <- function(n, ts.length, freq, seasonal, x0, max.fit
     if (seasonal == 0) {
       c(rep(0, 10))
     } else if (seasonal == 1) {
-      c(rep(ifelse(nsdiffs(x0) == 1, -0.5, -1), 12), rep(0, 5))
+      c(rep(c(ifelse(ndiffs(x0) >= 1, -0.5, -1),
+            ifelse(nsdiffs(x0) == 1, -0.5, -1)),
+            times = 3, each = 2), rep(0, 5))
     } else {
       c(rep(0, 35))
     }
@@ -11,7 +13,9 @@ generate_ts_with_target_ts <- function(n, ts.length, freq, seasonal, x0, max.fit
     if (seasonal == 0) {
       c(rep(1, 10))
     } else if (seasonal == 1) {
-      c(rep(ifelse(nsdiffs(x0) == 1, 0.5, 1), 12), rep(0.5, 5))
+      c(rep(c(ifelse(ndiffs(x0) >= 1, 0.5, 1),
+              ifelse(nsdiffs(x0) == 1, 0.5, 1)),
+            times = 3, each = 2), rep(0.5, 5))
     } else {
       c(rep(1, 35))
     }
