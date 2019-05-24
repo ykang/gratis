54ad24b6e0cbb407604c3061b679741f8ac09e06
Yanfei Kang <yanfei.kang@outlook.com>
2019-05-24 19:39:12 +0800

    cluster run

--- R/generate_ts_with_target_ts.R
+++ R/generate_ts_with_target_ts.R
@@ -3,7 +3,7 @@ generate_ts_with_target_ts <- function(n, ts.length, freq, seasonal, x0, max.fit
     if (seasonal == 0) {
       c(rep(0, 10))
     } else if (seasonal == 1) {
-      c(rep(ifelse(nsdiffs(x0) == 1, -0.75, -0.75), 12), rep(0, 5))
+      c(rep(ifelse(nsdiffs(x0) == 1, -0.5, -1), 12), rep(0, 5))
     } else {
       c(rep(0, 35))
     }
@@ -11,7 +11,7 @@ generate_ts_with_target_ts <- function(n, ts.length, freq, seasonal, x0, max.fit
     if (seasonal == 0) {
       c(rep(1, 10))
     } else if (seasonal == 1) {
-      c(rep(ifelse(nsdiffs(x0) == 1, 0.75, 0.75), 12), rep(0.5, 5))
+      c(rep(ifelse(nsdiffs(x0) == 1, 0.5, 1), 12), rep(0.5, 5))
     } else {
       c(rep(1, 35))
     }
