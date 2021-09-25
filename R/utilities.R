4d40c3282a833af90f2a536e45cc5f2c45d2f01e
Rob J Hyndman <robjhyndman@gmail.com>
2021-09-25 14:41:53 +1000

    Removed unused functions

--- R/utilities.R
+++ R/utilities.R
@@ -32,8 +32,7 @@ pi_coefficients <- function(ar = 0, d = 0L, ma = 0, sar = 0, D = 0L, sma = 0, m 
     seasonal_poly <- numeric(m * P)
     seasonal_poly[m * seq(P)] <- sar
     sar <- polynomial(c(1, -seasonal_poly)) * polynomial(c(1, rep(0, m - 1), -1))^D
-  }
-  else {
+  } else {
     sar <- 1
   }
 
@@ -46,8 +45,7 @@ pi_coefficients <- function(ar = 0, d = 0L, ma = 0, sar = 0, D = 0L, sma = 0, m 
     seasonal_poly <- numeric(m * Q)
     seasonal_poly[m * seq(Q)] <- sma
     sma <- polynomial(c(1, seasonal_poly))
-  }
-  else {
+  } else {
     sma <- 1
   }
 
@@ -59,8 +57,9 @@ pi_coefficients <- function(ar = 0, d = 0L, ma = 0, sar = 0, D = 0L, sma = 0, m 
   phi <- -c(coef(ar * sar)[-1], numeric(n))
   q <- length(theta)
   pie <- c(numeric(q), 1, numeric(n))
-  for (j in seq(n))
+  for (j in seq(n)) {
     pie[j + q + 1L] <- -phi[j] - sum(theta * pie[(q:1L) + j])
+  }
   pie <- pie[(0L:n) + q + 1L]
 
   # Return up to last element greater than tol
@@ -69,89 +68,45 @@ pi_coefficients <- function(ar = 0, d = 0L, ma = 0, sar = 0, D = 0L, sma = 0, m 
   return(-pie[-1])
 }
 
-# Compute pi coefficients from ARIMA model
-#  @param object An object of class "Arima"
-#
-# @return A vector of AR coefficients
-# @author Rob J Hyndman
-#
-# arinf <- function(object) {
-#   if (!("Arima" %in% class(object))) {
-#     stop("Argument should be an ARIMA object")
-#   }
-#   pi_coefficients(
-#     ar = object$model$phi, ma = object$model$theta,
-#     d = object$arma[6], D = object$arma[7], m = object$arma[5]
-#   )
-# }
-# 
-# # library(forecast)
-# # USAccDeaths %>% auto.arima %>% arinf %>% plot
-# # lynx %>% auto.arima %>% arinf %>% plot
-
-# Set the number of seasonal differences for yearly data to be -1.
-#
-# @param x Univariate time series or numerical vector
-nsdiffs1 <- function(x) {
-  c(nsdiffs = ifelse(frequency(x) == 1L, -1, forecast::nsdiffs(x)))
-}
-
-
-nroot <- function(x,n){
-  abs(x)^(1/n)*sign(x)
-}
-
-corrtemporder1 <- function (x, y) {
-  p <- length(x)
-  sum((x[2:p] - x[1:(p - 1)]) * (y[2:p] - y[1:(p - 1)]))/(sqrt(sum((x[2:p] -
-                                                                      x[1:(p - 1)])^2)) * sqrt(sum((y[2:p] - y[1:(p - 1)])^2)))
-}
-
-diss.cort <- function (x, y, k = 2){
-  corrt <- corrtemporder1(x, y)
-  typedist <- as.numeric(dist(rbind(x, y)))
-  (2/(1 + exp(k * corrt))) * typedist
-}
-
-scalets01 <- function(x){
-  n <- length(x)
-  scaledx <- as.numeric((x-min(x))/(max(x)-min(x)))
-  y <- as.ts(scaledx)
-  tsp(y) <- tsp(x)
-  return(y)
-}
-
-SeasonalityTest <- function(input, ppy){
-  if (length(input)<3*ppy){
+SeasonalityTest <- function(input, ppy) {
+  if (length(input) < 3 * ppy) {
     test_seasonal <- FALSE
-  }else{
+  } else {
     xacf <- acf(input, plot = FALSE)$acf[-1, 1, 1]
-    clim <- 1.645/sqrt(length(input)) * sqrt(cumsum(c(1, 2 * xacf^2)))
-    test_seasonal <- ( abs(xacf[ppy]) > clim[ppy] )
-    if (is.na(test_seasonal)==TRUE){ test_seasonal <- FALSE }
+    clim <- 1.645 / sqrt(length(input)) * sqrt(cumsum(c(1, 2 * xacf^2)))
+    test_seasonal <- (abs(xacf[ppy]) > clim[ppy])
+    if (is.na(test_seasonal)) {
+      test_seasonal <- FALSE
+    }
   }
   return(test_seasonal)
 }
-Smoothing_ts2 <- function(x, spanw, fh){
-  ppy <- frequency(x) ; ST <- F ; trend <- 1:length(x)
-  if (ppy>1){ ST <- SeasonalityTest(x,ppy) }
-  if (ST==T){
-    lambda <- BoxCox.lambda(x,lower=0,upper=1)
+
+Smoothing_ts2 <- function(x, spanw, fh) {
+  ppy <- frequency(x)
+  trend <- seq_len(x)
+  if (ppy > 1) {
+    ST <- SeasonalityTest(x, ppy)
+  } else {
+    ST <- FALSE
+  }
+  if (ST) {
+    lambda <- BoxCox.lambda(x, lower = 0, upper = 1)
     bc.x <- as.numeric(BoxCox(x, lambda))
-    seasonal <- stl(ts(bc.x, frequency = ppy), "per")$time.series[, 1]
+    seasonal <- stl(ts(bc.x, frequency = ppy), s.window = "periodic")$time.series[, 1]
     bc.x <- bc.x - as.numeric(seasonal)
-    x <- as.numeric(InvBoxCox(bc.x, lambda))+x-x
-    suppressWarnings(x.loess <- loess(x ~ trend, span = spanw/length(x), degree = 1))
-    x <- as.numeric(x.loess$fitted)+x-x
+    x <- as.numeric(InvBoxCox(bc.x, lambda)) + x - x
+    suppressWarnings(x.loess <- loess(x ~ trend, span = spanw / length(x), degree = 1))
+    x <- as.numeric(x.loess$fitted) + x - x
     SIin <- seasonal
-    SIout <- head(rep(seasonal[(length(seasonal)-ppy+1):length(seasonal)], fh), fh)
-  }else{
-    suppressWarnings(x.loess <- loess(x ~ trend, span = spanw/length(x), degree = 1))
-    x <- as.numeric(x.loess$fitted)+x-x
+    SIout <- head(rep(seasonal[(length(seasonal) - ppy + 1):length(seasonal)], fh), fh)
+  } else {
+    suppressWarnings(x.loess <- loess(x ~ trend, span = spanw / length(x), degree = 1))
+    x <- as.numeric(x.loess$fitted) + x - x
     SIin <- rep(0, length(x))
     SIout <- rep(0, fh)
     lambda <- 1
   }
-  output <- list(series=x, seasonalIn=SIin, seasonal=SIout, lambda=lambda)
+  output <- list(series = x, seasonalIn = SIin, seasonal = SIout, lambda = lambda)
   return(output)
 }
