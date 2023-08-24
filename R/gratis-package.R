b8a757eea3c99afdfa605267b9989aa8573611f2
Feng Li <m@feng.li>
2023-08-24 17:48:36 +0800

    Fix docType issue

--- R/gratis-package.R
+++ R/gratis-package.R
@@ -3,7 +3,7 @@
 #' The gratis package generates synthetic time series data based on various
 #' univariate time series models including MAR, ARIMA and ETS processes.
 #'
-#' @name gratis
+#' @aliases gratis-package
 #' @importFrom stats as.ts coef dlnorm plnorm qlnorm rlnorm rmultinom rnorm runif stl ts
 #' @importFrom stats diffinv fivenum frequency na.exclude na.omit sd window simulate
 #' @importFrom generics generate
